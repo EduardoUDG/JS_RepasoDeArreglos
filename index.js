2413f329ed94f7f49599ad9473c738f351b7902a
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2021-06-08 13:57:25 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const array = ['Juan', 'Pepe', 'Toño']
+const ArrayIteration = array.entries()
+console.log(ArrayIteration.next().value);
+console.log(ArrayIteration.next().value);
+console.log(ArrayIteration.next().value);
+console.log(ArrayIteration.next().value);
+console.log(ArrayIteration.next().value);
+
